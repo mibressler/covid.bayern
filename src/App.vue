06650ef22f8267ddc13cb71c8fbbc07ca7789163
Michael Bressler <53796824+mbressler@users.noreply.github.com>
2021-01-29 15:02:02 +0100

    up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Content msg="Welcome me to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from './components/Content.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Content
   }
 }
 </script>
